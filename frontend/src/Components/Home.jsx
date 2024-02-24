60ee12c2aaf66d014464cf4db7646e6d48f9f3f1
Divy Mav <divymav5@gmail.com>
2024-02-24 22:28:58 +0530

    Responsiveness added + Footer

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,13 +1,33 @@
-import React, { useRef } from 'react';
-import back from '../assets/EduManager.png'
+import React, { useEffect, useRef } from 'react';
+import back from '../assets/EduManager.png';
+import backres from '../assets/EduManagerResponsive.png';
 import Navbar from './Navbar';
 
 const Home = () => {
 
+    const bgImg = useRef();
+
+    const changeBG = () => {
+        if (window.innerWidth < 1000) {
+            bgImg.current.src = backres;
+        } else {
+            bgImg.current.src = back;
+        }
+    }
+
+    useEffect(() => {
+        changeBG();
+        window.addEventListener('resize', changeBG);
+
+        return () => {
+            window.removeEventListener('resize', changeBG);
+        };
+    }, []);
+
     return (
         <div>
             <Navbar />
-            <img src={back} alt="background" srcset="" />
+            <img src={back} alt="background" ref={bgImg} />
             <div className="btn-grp">
                 <button className='experience-it1'>Experience It</button>
                 <button className='explore-more1'>Explore More</button>
@@ -17,6 +37,9 @@ const Home = () => {
             </div>
             <style>
                 {`
+                body{
+                    background-color: #e3f5ff;
+                }
                 .btn-grp{
                     display:flex;
                     justify-content:center;
@@ -66,10 +89,33 @@ const Home = () => {
                     -webkit-user-select: none;
                     -ms-user-select: none;
                 }
+                @media screen and (max-width:1500px){
+                    img{
+                        width:100%;
+                    }
+                    .experience-it1{
+                        top:500px;
+                        right:400px;
+                    }
+                    .explore-more1{
+                        top:1100px;
+                    }
+                    .experience-it2{
+                        top:1650px;
+                        right:400px;
+                    }
+                    .explore-more2{
+                        top:2250px;
+                    }
+                    .experience-it3{
+                        top:2830px;
+                        right:400px;
+                    }
+                    }
                 `}
             </style>
         </div>
     )
 }
 
-export default Home
+export default Home;
