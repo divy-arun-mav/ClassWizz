7c58fd3ea479b497dbf85c52d479ff35344038fd
Ashish48Maurya <maurya.48.ashish.com>
2024-02-25 00:59:54 +0530

    SignUP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState,useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import Allocate from './Components/Allocate-class';
 import Navbar from './Components/Navbar';
 import Login from './Components/Login';
 import AdminRegister from './Components/AdminRegister';
@@ -11,19 +9,17 @@ import StudentRegister from './Components/StudentRegister';
 import TeacherRegister from './Components/TeacherRegister';
 import Home from './Components/Home';
 import ClassRoom from './Components/ClassRoom';
-import { useState, useEffect } from 'react';
 import { useAuth } from './Components/store/auth';
 
 function App() {
   const { person } = useAuth();
   const [user, setUser] = useState('');
 
-  let ans;
   useEffect(() => {
-    ans = localStorage.getItem('USER');
+    const ans = localStorage.getItem('USER');
     setUser(ans ? JSON.parse(ans) : null);
   }, []);
-  console.log("FROM APP.JS: ",person);
+  // console.log("PERSON",person);
 
   return (
     <>
@@ -31,17 +27,10 @@ function App() {
         <Route exact path='/navbar' element={<Navbar user={user} />} />
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
-
         <Route exact path='/classroom' element={<ClassRoom />} />
-        <Route exact path='/studentregister' element={<StudentRegister />} />
-        <Route exact path='/teacherregister' element={<TeacherRegister />} />
-        <Route exact path='/adminregister' element={<AdminRegister />} />
-        
-        { person==="Student" && <Route exact path='/studentregister' element={<StudentRegister />} />}
-        { person==="Teacher" && <Route exact path='/teacherregister' element={<TeacherRegister />} />}
-        { person==="Admin" && <Route exact path='/adminregister' element={<AdminRegister />} />}
-
-
+        {person === 'Student' && <Route exact path='/studentregister' element={<StudentRegister />} />}
+        {person === 'Teacher' && <Route exact path='/teacherregister' element={<TeacherRegister />} />}
+        {person === 'Admin' && <Route exact path='/adminregister' element={<AdminRegister />} />}
       </Routes>
       <ToastContainer />
     </>
